3ffa85882228d7777e4e5ddd411c74f410ca6652
liwei <564141923@qq.com>
2017-04-13 14:00:23 +0800

    Signed-off-by: liwei <564141923@qq.com>添加仓库

---
+++ app/src/main/java/com/liwei/train/MainActivity.java
@@ -0,0 +1,13 @@
+package com.liwei.train;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
